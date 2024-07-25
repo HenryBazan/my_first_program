7aa91ed9fc63b3072f6510b5bb023c0d27820d09
Henry Bazan <109365110+HenryBazan@users.noreply.github.com>
2024-07-25 16:18:53 -0500

    Commiting Agent

---
+++ my_first_programclear/target/debug/deps/my_first_programclear.d
@@ -0,0 +1,5 @@
+C:\Users\henry\OneDrive\Desktop\Hub\UTRGV\summer24\sys programming\rust\my_first_program\my_first_programclear\target\debug\deps\my_first_programclear.exe: src/main.rs
+
+C:\Users\henry\OneDrive\Desktop\Hub\UTRGV\summer24\sys programming\rust\my_first_program\my_first_programclear\target\debug\deps\my_first_programclear.d: src/main.rs
+
+src/main.rs:
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::process::Command;
+use std::io::{self, Read, Write};
+
+fn executing_os_commands_linux(command_full: &str) {
+
+    let parts: Vec<&str> = command_full.split_whitespace().collect();
+    let actual_command = parts[0];
+    let arg1 = parts[1];
+
+    let output = Command::new(actual_command)
+        .arg(arg1)
+        .output()
+        //.expect("Failed to execute command");
+        .unwrap();
+    //println!("This is the out put \n {:?} output done \n", output);
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn accept_linux_command_from_user()->String{
+    let mut buffer = String::new();
+    println!("Type command: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let tcommand = buffer.trim().to_string();
+    buffer.clear();
+
+    tcommand
+
+
+}
+
+fn main(){
+
+    let full_command:String = accept_linux_command_from_user();
+
+    executing_os_commands_linux(&full_command);
+    //executing_os_commands_linux("echo hello");
+    //executing_os_commands_linux("ls -la");
+}
